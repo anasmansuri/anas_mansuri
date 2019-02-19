c5e1e47ff1ec4d8eda2da14677b9aaf16514a869
anasmansuri <44845875+anasmansuri@users.noreply.github.com>
2019-02-19 13:44:03 +0530

    Update sorting.js

--- sorting.js
+++ sorting.js
@@ -1,3 +1,4 @@
+//for
 function sort(num){
     for(var i=0;i<num.length;i++){
         for(var j=i+1;j<num.length;j++){
@@ -14,7 +15,7 @@ function sort(num){
 var x=[5,4,6,8,7];
 var ans=sort(x);
 
-/*
+//forEach
 var number=[201,3,192,5,4];
 
 number.forEach(function (param,k,ar) {
@@ -28,5 +29,20 @@ number.forEach(function (param,k,ar) {
         }
     }
     console.log(ar[k]);
-});*/
+});
+//map
+var ar=[50,45,2,15,18,17];
+var c=ar.map(function (param,k,ar) {
+    for(var i=0;i<ar.length;i++){
+        for(var j=i+1;j<ar.length;j++){
+            if(ar[j]<ar[i]){
+                temp=ar[j];
+                ar[j]=ar[i];
+                ar[i]=temp;
+            }
+        }
+    }
+    return ar[k];
+});
+console.log(c);
  
